bca453ca3423817b89fef1c0cdcf34651a7599ef
surajv1986 <surajv1986@gmail.com>
2015-08-12 08:58:50 +0530

    7th commit has errors

--- Application/App.js
+++ Application/App.js
@@ -1,1 +1,23 @@
-angular.module('RideApp',['ui.bootstrap']);
+angular.module('RideApp',['ui.bootstrap','ui.router'])
+.config(['$stateProvider, $urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+   $stateProvider
+    .state('Home', {
+      url: "/Home",
+      templateUrl: "partials/Home.html",
+      controller:"SearchCtrl"
+    })
+
+    $stateProvider
+    .state('Dashboard.html', {
+      url: "/Dashboard.html",
+      templateUrl: "partials/Dashboard.html"
+    })
+
+    $stateProvider.otherwise('/Home');
+
+ }
+]);
+
+
+
